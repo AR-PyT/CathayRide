68ee7a86a4d9edb8e2fc062e468fce7ec3aadec6
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2024-11-07 02:56:18 +0800

    Add customer grouping

--- CathayRide/grouping.py
+++ CathayRide/grouping.py
@@ -25,24 +25,47 @@ class Grouping:
         dist = np.sqrt(np.square(cords - cords[:, None]))
         indices = np.argmin(dist, axis=1)
 
-        
         # Goal is to minimize the additional distance while ensuring maximum baggage
         taxiList.sort(key = lambda taxi: taxi.costMultilpier)
         left = len(customers)
+        completedUsers = {}
+        groups = []
         for taxi in taxiList:
-            if left <= 0:
+            if len(completedUsers) == len(customers):
                 break
-
-            taxi.maxCapacity
-
+            groups.append(findCloseCustomer(baggage, indices, hailers, taxi.maxLuggage, taxi.maxCapacity, completedUsers))
+            completedUsers = completedUsers.union(set(groups[-1]))
+        
+        return groups
 
     # returned customers are with min dist while having baggage and hailers bounded by c1, c2
     # Use exact constraint for c2 while baggage can vary
     @staticmethod
-    def findCloseCustomer(dist, baggage, idx, hailers, baggageConstraint, capacityConstraint):
-        
+    def findCloseCustomer(baggage, idx, hailers, baggageConstraint, capacityConstraint, completedUsers):        
+        blackList = {index for index in idx[0] if hailers[index] >= capacityConstraint}
+        blackList = blackList.union(completedUsers)
+        if len(idx) - len(blackList) < capacityConstraint:
+            return findCloseCustomer(baggage, idx, hailers, baggageConstraint, capacityConstraint - 1, completedUsers)
+
         for index in idx:
-            hailer = hailers[idx]
-            bag = baggage[idx]
+            index = list(set(index).difference(blackList))
+            curBaggage = baggage[index]
+            curSum = sum(curBaggage[:capacityConstraint])
+            selected = [x for x in range(capacityConstraint)]
+            remaining = [x for x in range(capacityConstraint, len(baggage))]
+            while curSum > baggageConstraint:
+                moveIdx = math.argmax(baggage[selected])
+                curSum -= baggage[selected[moveIdx]]
+                if len(remaining) == 0:
+                    continue
+                selected[moveIdx] = remaining.pop(0)
+                curSum += baggage[selected[moveIdx]]
+            return selected
+            
+    
+
+
+
+        
 
 
